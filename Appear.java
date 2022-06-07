ae662db159719e5e847e226f126c326a1f491c58
Vigneshmani1411 <104953836+Vigneshmani1411@users.noreply.github.com>
2022-06-07 08:48:27 +0530

    Add files via upload

---
+++ Appear.java
@@ -0,0 +1,35 @@
+package week5.day5;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Appear {
+
+	public static void main(String[] args) throws IOException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/appear.html");
+		driver.manage().window().maximize();
+		
+		WebElement ele = driver.findElement(By.id("btn"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.visibilityOf(ele));
+		
+		File source = driver.getScreenshotAs(OutputType.FILE);
+		File destination = new File("./snap/appear.png");
+		FileUtils.copyFile(source, destination);
+		
+		
+	}
+}
